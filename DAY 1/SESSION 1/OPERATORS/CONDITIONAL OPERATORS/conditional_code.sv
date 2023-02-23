ba0e3aaba3a88ba4252031ec57aba97def4aa76f
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-23 15:52:02 +0530

    Create conditional_code.sv

---
+++ DAY 1/SESSION 1/OPERATORS/CONDITIONAL OPERATORS/conditional_code.sv
@@ -0,0 +1,20 @@
+module conditional_code;
+
+int c,d;
+string y;
+string a,b;
+initial begin
+
+a = "true";
+b = "false";
+c=4'b1011;
+d=4'b1011;
+
+$display("\n \t the value of c is %0b",c);
+
+$display("\n \t the value of d is %0b",d);
+
+y=c>d?a:b;
+$display("\n \t the conditional output is %0b",y);
+end
+endmodule 
