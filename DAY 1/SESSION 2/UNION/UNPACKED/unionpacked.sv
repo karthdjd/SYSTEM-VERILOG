833c7fef158a6f1b943649bf0037c775fda1f605
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-24 12:32:57 +0530

    Create unionpacked.sv

---
+++ DAY 1/SESSION 2/UNION/UNPACKED/unionpacked.sv
@@ -0,0 +1,20 @@
+
+typedef union packed {
+  bit [7:0] B;
+  logic [7:0] C  ;
+} ABC_u;
+module unionpacked;
+  
+  ABC_u abc; 
+  initial begin 
+    
+
+  abc.B = 'hab;
+    $display("\n abc.B = %0h" ,abc.B);
+ 
+ abc.C = 'hcd;
+    $display("\n abc.C = %0h" ,abc.C); 
+    $display("\n size is:",$bits(ABC_u));
+
+  end 
+endmodule;
