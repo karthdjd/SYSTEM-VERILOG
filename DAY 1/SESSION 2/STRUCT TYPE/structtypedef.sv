bde018e0a61fe9c5400f81589054e25b95a81e7b
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-24 12:08:05 +0530

    Create structtypedef.sv

---
+++ DAY 1/SESSION 2/STRUCT TYPE/structtypedef.sv
@@ -0,0 +1,31 @@
+module structtypedef;
+
+
+typedef struct {   
+	string name;
+	byte id;
+      longint li;
+	
+}personal_details_s;   
+
+ 
+personal_details_s  details;
+
+initial begin
+
+
+details.name= "Melbin";
+details.id = 23;
+details.li = 22;
+
+
+ 
+         
+$display("\n details.name = %p" ,details.name); 
+$display("\n details.id = %p" ,details.id);
+$display("\n details.li = %p" ,details.li);
+
+
+$display("\n Bitstream size of personal_details_s: %0d", $bits(details));
+end 
+endmodule;
