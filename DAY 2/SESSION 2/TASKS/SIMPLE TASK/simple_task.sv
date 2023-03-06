507df3882e536def6f560b559caa2580d05eee2d
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-03-06 17:01:44 +0530

    Create simple_task.sv

---
+++ DAY 2/SESSION 2/TASKS/SIMPLE TASK/simple_task.sv
@@ -0,0 +1,15 @@
+module simple_task();
+  
+  int multiplicand=5,multiplicator=7,result;
+  initial
+  begin
+    $display("\t ----output of simple task calling----");
+  mul(multiplicand,multiplicator,result);
+  $display("\t @ %0t ns , %0d X %0d = %0d",$time,multiplicand,multiplicator,result);
+  end
+
+  task mul(input int var1,var2,output int res);
+    #2 res=var1*var2;
+  endtask
+
+endmodule
