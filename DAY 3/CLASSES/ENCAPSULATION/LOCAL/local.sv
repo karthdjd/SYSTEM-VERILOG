c9365fb34beb11586af62c72bce919b389572e05
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-03-07 07:10:04 +0530

    Create local.sv

---
+++ DAY 3/CLASSES/ENCAPSULATION/LOCAL/local.sv
@@ -0,0 +1,40 @@
+class parent;
+  int a,b;
+  local int c;
+  local int d = 5;
+
+  function int sum(int val1,val2);
+    c = val1 + val2;
+    return c;
+  endfunction:sum
+
+  function void display();
+    $display("sum = %0d",c);
+    $display("d = %0d",d);
+  endfunction:display
+
+endclass:parent
+
+class child extends parent;
+
+  function void disp();
+   
+  endfunction
+
+endclass:child
+
+
+module encap();
+
+  parent p;
+  child c1;
+  int e;
+
+  initial begin
+    p = new();
+    c1 = new();
+    e = p.sum(5,10);
+    p.display();
+   end
+
+endmodule
